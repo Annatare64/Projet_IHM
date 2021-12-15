8e24a93039ce693ad6d3a4b462f4b302e9b618a2
BenLeBoss <49184923+BenLeBoss@users.noreply.github.com>
2021-12-15 14:30:18 +0100

    boi

--- src/EnTete.js
+++ src/EnTete.js
@@ -1,62 +1,62 @@
-import React from "react";
-import { render } from "react-dom";
-import ReactDOM from "react-dom";
+import React from "react"
+import {render} from "react-dom"
+import ReactDOM from "react-dom"
 
-import ExoVocab from "./AtelierExercices/ExoVocabulaires";
-import ExoGramm from "./AtelierExercices/ExoGrammaires";
-import ExoConj from "./AtelierExercices/ExoConjugaison";
-import Magazines from "./Magazines/magazines";
-import Accueil from "./Accueil/accueil";
-import Guide from "./Outils/Guide";
-import Lexique from "./Outils/Lexique";
-import { Link, NavLink } from "react-router-dom";
-import "./CSS/Styles_EnTete.css";
+import ExoVocab from "./AtelierExercices/ExoVocabulaires"
+import ExoGramm from "./AtelierExercices/ExoGrammaires"
+import ExoConj from "./AtelierExercices/ExoConjugaison"
+import Magazines from "./Magazines/magazines"
+import Accueil from "./Accueil/accueil"
+import Guide from "./Outils/Guide"
+import Lexique from "./Outils/Lexique"
+import {Link, NavLink} from "react-router-dom"
+import "./CSS/Styles_EnTete.css"
 
 class EnTete extends React.Component {
-  Site = "English Learn";
+	Site = "English Learn"
 
-  render() {
-    return (
-      <div className="En-tete1">
-        <h1 className="Titre">&ensp;{this.Site}</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/Accueil">Accueil</Link>
-            </li>
-            <li>
-              <Link to="/Magazines">Magazines &ensp;</Link>
-            </li>
-            <li className="deroulant">
-              <a>Atelier &ensp;</a>
-              <ul className="sous">
-                <li>
-                  <Link to="/ExoGramm">Grammaire</Link>
-                </li>
-                <li>
-                  <Link to="/ExoConj">Conjugaison</Link>
-                </li>
-                <li>
-                  <Link to="/ExoVocab">Vocabulaire</Link>
-                </li>
-              </ul>
-            </li>
-            <li className="deroulant">
-              <a>Outils&ensp;</a>
-              <ul className="sous">
-                <li>
-                  <Link to="/Lexique">Lexique</Link>
-                </li>
-                <li>
-                  <Link to="/Guide">Guide grammatical</Link>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="En-tete1">
+				<h1 className="Titre">&ensp;{this.Site}</h1>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/Accueil">Acc</Link>
+						</li>
+						<li>
+							<Link to="/Magazines">Magazines &ensp;</Link>
+						</li>
+						<li className="deroulant">
+							<a>Atelier &ensp;</a>
+							<ul className="sous">
+								<li>
+									<Link to="/ExoGramm">Grammaire</Link>
+								</li>
+								<li>
+									<Link to="/ExoConj">Conjugaison</Link>
+								</li>
+								<li>
+									<Link to="/ExoVocab">Vocabulaire</Link>
+								</li>
+							</ul>
+						</li>
+						<li className="deroulant">
+							<a>Outils&ensp;</a>
+							<ul className="sous">
+								<li>
+									<Link to="/Lexique">Lexique</Link>
+								</li>
+								<li>
+									<Link to="/Guide">Guide grammatical</Link>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		)
+	}
 }
 
-export default EnTete;
+export default EnTete
